9e64c293d43ba957cf1fef78b5b6161385c4fe2b
tal.green <tal.green@controlup.com>
2022-04-10 15:17:56 +0300

    Adding basic fixture

--- tests_ui_layout/conftest.py
+++ tests_ui_layout/conftest.py
@@ -3,10 +3,7 @@ from playwright.sync_api import Playwright
 
 
 @pytest.fixture
-def set_up(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def set_up(page):
     page.set_default_timeout(30000)
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
 
